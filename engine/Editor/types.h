a824982b763fc63cdb48589ca25a7ab3251705f5
standtall007 <davidm@taorh.dk>
2016-05-27 15:35:50 +0200

    Update #5
    Changed the entire folder structure so that i will hopefully not get
    into trouble legally.
    Also i have optimized the 3D scene window / pane to be able to contain
    approx. 5.38 times more polygons!
    And last but not least, now you can also use the project explorer so
    when you're selecting an object from the explorer, then it selects that
    particular object in the 3d scene! And the objects also have a boundary
    box to see which is selected!

--- engine/Editor/types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef WIN32
-#include <unistd.h>
-#endif
-
-typedef unsigned int uint;
-typedef unsigned short ushort;
-typedef unsigned char uchar;
--- engine/Player/resourcemanager.cpp
+++
@@ -1,8 +0,0 @@
-#include "resourcemanager.h"
-
-TextureCache ResourceManager::m_textureCache;
-
-GLTexture ResourceManager::getTexture(std::string texturePath)
-{
-	return m_textureCache.GetTexture(texturePath);
-}
