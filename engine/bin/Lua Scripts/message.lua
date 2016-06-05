fb242826623882a677445d063b076b98dcf81cd4
standtall007 <davidm@taorh.dk>
2016-06-05 17:13:30 +0200

    Minor Update #17
    Just quickly added two Lua functions which i have made two lua scripts
    to show :) it works only in the player for now!

---
+++ engine/bin/Lua Scripts/message.lua
@@ -0,0 +1,3 @@
+--This example shows how to open a simple message box.
+
+Game.Message("This is just a simple message test!")
---
+++ engine/bin/do_not_delete.lua
@@ -0,0 +1,6 @@
+--You can use full paths like (C:/Nutcrackz/bin/do_not_delete.lua)
+--And you can also do it like below
+--But you can also use \\ instead of / in your filepaths!
+--Just remember that if you choose to do it like below, it is very important that you use . as your first symbol
+--before using / or \\ !
+LoadScript("./Lua Scripts/message.lua")
